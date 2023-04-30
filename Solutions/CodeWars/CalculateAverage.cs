566e1b80168ebde92e4ec1818a8ee4067c053062
FabianAronsson <fabianaronsson2@gmail.com>
2023-04-30 20:42:18 +0200

    Add 'Calculate average' solution

---
+++ Solutions/CodeWars/CalculateAverage.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeWars;
+//https://www.codewars.com/kata/57a2013acf1fa5bfc4000921
+public static class CalculateAverage
+{
+    public static double Solution(double[] array)
+    {
+        return (array.Length == 0 ? 0 : array.Sum() / array.Length);
+    }
+}
--- Solutions/CodeWars/Program.cs
+++ Solutions/CodeWars/Program.cs
@@ -12,3 +12,4 @@ Console.WriteLine("CodeWars Solutions \n Uncomment to test! \n");
 //DuplicateEncoder.Solution("din");
 //DecodetheMorsecode.Solution(".... . -.--   .--- ..- -.. .");
 //Buildasquare.Solution(9);
+//CalculateAverage.Solution(new double[] { 17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16 });
