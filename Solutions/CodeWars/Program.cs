480dae5bb410f313f43a4d9851bf73b4bc4f63ad
FabianAronsson <fabianaronsson2@gmail.com>
2023-04-30 20:16:23 +0200

    Add 'Regex validate PIN code' solution

--- Solutions/CodeWars/Program.cs
+++ Solutions/CodeWars/Program.cs
@@ -5,3 +5,4 @@
 //DetectPanagram.Solution("The quick brown fox jumps over the lazy dog");
 //Isograms.Solution("Dermatoglyphics");
 //YourOrderPlease.Solution("is2 Thi1s T4est 3a");
+//RegexValidatePINcode.Solution("1234");
---
+++ Solutions/CodeWars/RegexValidatePINcode.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeWars;
+
+//https://www.codewars.com/kata/55f8a9c06c018a0d6e000132
+public class RegexValidatePINcode
+{
+    public static bool Solution(string pin)
+    {
+        if (Regex.IsMatch(pin, @"[\D+]") != true && (pin.Length == 6 || pin.Length == 4))
+        {
+            return true;
+        }
+        return false;
+    }
+}
