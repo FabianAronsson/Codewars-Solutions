d7898c6f7291b4eec4957c9cf630c1e24e6dfe02
FabianAronsson <fabianaronsson2@gmail.com>
2023-04-30 20:12:58 +0200

    Add 'Isograms' solution

--- Solutions/CodeWars/Program.cs
+++ Solutions/CodeWars/Program.cs
@@ -3,3 +3,4 @@
 //Multiply.Solution(1, 2);
 //Maskify.Solution("4556364607935616");
 //DetectPanagram.Solution("The quick brown fox jumps over the lazy dog");
+//Isograms.Solution("Dermatoglyphics");
