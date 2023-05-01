d6ef5f0b2ea0c2ea312ab599e40a39f9f6c15b15
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-01 20:11:30 +0200

    Add 'Adding Big Numbers' solution

---
+++ Solutions/CodeWars/AddingBigNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace CodeWars;
+
+//https://www.codewars.com/kata/525f4206b73515bffb000b21
+public static class AddingBigNumbers
+{
+    public static string Solution(string a, string b)
+    {
+        return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+    }
+}
--- Solutions/CodeWars/Program.cs
+++ Solutions/CodeWars/Program.cs
@@ -18,3 +18,4 @@ Console.WriteLine("CodeWars Solutions \n Uncomment to test! \n");
 //StopgninnipSMyswroW.Solution("Hey fellow warriors");
 //RangeExtraction.Solution(new [] {-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20});
 //ROT13.Solution("EBG13 rknzcyr.");
+//AddingBigNumbers.Solution("12314123123123123123", "98109091203948109274089712");
